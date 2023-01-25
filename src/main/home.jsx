baad78a6be6146e1ab4db9442f5fbf3524146ee7
MwangiNelson <nelsonmwangi197@gmail.com>
2023-01-26 00:15:38 +0300

    yeah

--- src/main/home.jsx
+++ src/main/home.jsx
@@ -6,14 +6,22 @@ function Home() {
         <section className='home'>
             <nav className="home-nav-bar">
                 <ul className='flex-row'>
-                    <li><a href=""><i class="fa-solid fa-magnifying-glass"></i> FIND RECIPES</a></li>
+                    <li><a href=""><i className="fa-solid fa-magnifying-glass"></i> FIND RECIPES</a></li>
                     <li><a href="">RECIPE GENERATOR</a></li>
                     <li><a href="">POST RECIPE</a></li>
                     <li><a href="">CREATE ACCOUNT</a></li>
                 </ul>
             </nav>
 
-            
+            <div className="main-body">
+                <div className="intro-container">
+                    <h3>HELLO THERE!</h3>
+                    <p>
+                        Welcome to <b>RECETTE</b> beta version <br />
+                        As a developer, the only thing I can cook is spaghetti code
+                    </p>
+                </div>
+            </div>
         </section>
     )
 }
